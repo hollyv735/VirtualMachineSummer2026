63c4a64911314e32a5e648f6741dc5fa2fed1d49
Holly Vogt <holly@vogts.us>
2026-06-15 09:34:40 -0400

    Some work on VMX FIle

--- impl/src/main/java/com/kondra/vm/vmx/VmxReader.java
+++ impl/src/main/java/com/kondra/vm/vmx/VmxReader.java
@@ -107,7 +107,7 @@ public class VmxReader {
         int sSize = readInt();
         Section s = new Section(sOffset, sSize);
         byte[] content = new byte[sSize];
-        for(int i = 0; i<sSize-2; i++){
+        for(int i = 0; i<sSize; i++){
             content[i] = readByte();
         }
         s.setSection(content);
